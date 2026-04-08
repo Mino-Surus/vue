f28e2d0b0692d92a70ef41a3f34f9ac7b56371ee
Bubleks <gugugaga23166@gmail.com>
2026-04-08 16:40:49 +0500

    dsfm

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const arrs = ref([
+  {id: 1, tt:'hello' },
+  {id: 2, tt:'bye' },
+  {id: 3, tt:'play' }
+])
+</script>
+
+<template>
+  <ul>
+    <li v-for="arr in arrs">
+        {{ arrs.tt }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
+
