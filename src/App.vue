1049a898d917790154d0e50f0db54ad9e54d4a4f
Студент группы IS <is@sielom.local>
2026-04-10 17:55:19 +0500

    guvsfk

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,28 @@ const arrs = ref([
   {id: 2, tt:'bye' },
   {id: 3, tt:'play' }
 ])
+
+
+let deleteElement = (arr) => {
+  arrs.value = arrs.value.filter(a => a != arr)
+}
+
+let addItem = (arr) =>
+
 </script>
 
 <template>
+  <p> ПРивет медвед</p>
   <ul>
-    <li v-for="arr in arrs">
-        {{ arrs.tt }}
+    <li v-for="arr in arrs" :key="id">
+        {{ arr.tt }}
+        <button v-on:submit.prevent="deleteElement(arr)"></button>
     </li>
   </ul>
+  <form>
+    <input v-model="arrs">
+    <button @click="addItem"> Добавить </button>
+  </form>
 </template>
 
 <style scoped></style>
