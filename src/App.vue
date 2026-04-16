422b34bf05e6b463c805cb578e89ff77981d48af
Студент группы IS <is@sielom.local>
2026-04-16 16:31:30 +0500

    all

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,18 @@ let deleteElement = (arr) => {
   arrs.value = arrs.value.filter(a => a != arr)
 }
 
-let addItem = (arr) =>
+let addItem = (arr) => {
 
+}
+
+
+// import Child from "./Child.vue"
+// import {ref} from "vue"
+
+// const mail = ref("mm@mail.ru")
 </script>
 
 <template>
-  <p> ПРивет медвед</p>
   <ul>
     <li v-for="arr in arrs" :key="id">
         {{ arr.tt }}
@@ -28,6 +34,10 @@ let addItem = (arr) =>
     <input v-model="arrs">
     <button @click="addItem"> Добавить </button>
   </form>
+
+
+<!-- <Child :num="mail" /> -->
+
 </template>
 
 <style scoped></style>
---
+++ src/Child.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    msg: String,
+    default: "",
+    num: {
+        type: String,
+        required: true,
+        default: () => {
+            return "example@mail.ru"
+        },
+        validator: (value) => {
+        value.length > 5 && value.includes('@')
+        }
+    }
+})
+</script>
+
+<template>
+    <p>{{num}}</p>
+</template>
